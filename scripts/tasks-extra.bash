258cf40f5d00b8a0dcefed686fe9ade59c5a9f6b
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-10-29 17:47:49 +0200

    Update repositories URL

--- scripts/tasks-extra.bash
+++ scripts/tasks-extra.bash
@@ -2,8 +2,11 @@
 
 cat <<EOS
 
-specs-platform-core@package : mosaic-node-boot@package
-specs-platform-java@package : mosaic-node-boot@package
+specs-platform-core@package : mosaic-node-boot@package | mosaic-node-boot@publish
+specs-platform-java@package : mosaic-node-boot@package | mosaic-node-boot@publish
+
+specs-platform-core@publish :
+specs-platform-java@publish :
 
 EOS
 
